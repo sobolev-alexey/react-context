8d067cbcd22ba40d372361d4f19a697292752f42
Alexey Sobolev <sobolev@mail.de>
2019-04-04 12:28:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# react-context
-Alternative for redux, use of react context API 
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Check out the demo at
+https://react-context.firebaseapp.com/
---
+++ src/context/globalState.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import ShopContext from './shop-context';
+
+class GlobalState extends Component {
+  state = {
+    products: [
+      { id: 'product1', title: 'Product 1', price: 10 },
+      { id: 'product2', title: 'Product 2', price: 20 },
+      { id: 'product3', title: 'Product 3', price: 30 },
+    ],
+    cart: []
+  };
+
+  addProductToCart = product => {
+    console.log('Adding product', product);
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === product.id
+    );
+
+    if (updatedItemIndex < 0) {
+      updatedCart.push({ ...product, quantity: 1 });
+    } else {
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity++;
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState({ cart: updatedCart });
+  };
+
+  removeProductFromCart = productId => {
+    console.log('Removing product with id: ' + productId);
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === productId
+    );
+
+    const updatedItem = {
+      ...updatedCart[updatedItemIndex]
+    };
+    updatedItem.quantity--;
+    if (updatedItem.quantity <= 0) {
+      updatedCart.splice(updatedItemIndex, 1);
+    } else {
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    this.setState({ cart: updatedCart });
+  };
+
+  render() {
+    return (
+      <ShopContext.Provider
+        value={{
+          products: this.state.products,
+          cart: this.state.cart,
+          addProductToCart: this.addProductToCart,
+          removeProductFromCart: this.removeProductFromCart
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
+  }
+}
+
+export default GlobalState;
